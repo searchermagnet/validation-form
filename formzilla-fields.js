c9a9ed6f65f573e79c492840ef76ae55ee28fa7e
avoliva <adamvoliva@gmail.com>
2013-02-09 15:27:01 -0800

    First commit.

---
+++ formzilla-fields.js
@@ -0,0 +1,124 @@
+    var opt = {}
+    opt['rules'] =   StocKValidators = {
+        A_Zip: {
+                required: true,
+                maxlength: 5,
+                digits: true
+            },
+            N_First: {
+                required: true
+            },
+            N_Last: {
+                required: true
+            },
+            N_Middle: {
+                required: true
+            },
+            A_Street: {
+                required: true
+            },
+            A_City: {
+                required: true
+            },
+            D_Amount: {
+                required: true,
+                number: true
+            },
+            D_Payment: {
+                required: true,
+                number: true
+            },
+            D_InterestRate: {
+                required: true,
+                number: true
+            },
+            C_Email: {
+                required: true,
+                email: true
+            },
+            C_HomePhone: {
+                required: true,
+                digits: true,
+                maxlength: 10
+            },
+            C_HomePhone: {
+                required: true,
+                digits: true,
+                maxlength: 3
+            },
+            C_HomePhone2: {
+                required: true,
+                digits: true,
+                maxlength: 3
+            },
+            C_HomePhone3: {
+                required: true,
+                digits: true,
+                maxlength: 4
+            },
+            C_WorkPhone: {
+                required: true,
+                digits: true,
+                maxlength: 10
+            },
+            O_Age: {
+                required: true,
+                digits: true,
+                maxlength: 10
+            },
+            O_DateofBirth: {
+                required: true,
+                date: true
+            },
+            O_ContactTime: {
+                required: true
+            },
+            O_CreditScore: {
+                required: true
+            },
+            O_Income: {
+                required: true,
+                number: true
+            },
+            O_Comment: {
+                required: true
+            },
+            P_MortageExisting: {
+                required: true,
+                digits: true,
+                min: 10000,
+                max: 100000
+            },
+            P_ValueEstimated: {
+                required: true,
+                digits: true,
+                min: 10000,
+                max: 100000
+            },
+            P_Type: {
+                required: true
+            },
+            P_Interest: {
+                required: true,
+                digits: true
+            },
+            P_LoanType: {
+                required: true
+            },
+            P_Foreclosure: {
+                required: true
+            },
+            B_Vertical: {
+                required: true
+            },
+            B_Company: {
+                required: true
+            },
+            B_Website: {
+                required: true,
+                url: true
+            },
+            T_DebtAmount: {
+                required: true
+            }
+        }
---
+++ formzilla.js
@@ -0,0 +1,85 @@
+$(document).ready( function() {
+
+
+    //validates max-length in real time
+    $("input").keyup(function() {
+    switch (this.id) {
+        case 'A_Zip':
+            $(this).attr('maxlength', '5')
+            if (this.value.length >= 5) {
+                $(this).next().focus()
+                this.max
+            }
+            break                
+        }
+        case 'C_WorkPhone':
+        case 'C_HomePhone':
+            $(this).attr('maxlength', '10')
+            if (this.value.length >= 10) {
+                $(this).next().focus()
+                this.max
+            }
+            break                
+        }
+        case 'C_WorkPhone1':
+        case 'C_HomePhone1':
+            $(this).attr('maxlength', '3')
+            if (this.value.length >= 3) {
+                $(this).next().focus()
+                this.max
+            }
+            break                
+        }
+        case 'C_WorkPhone2':
+        case 'C_HomePhone2':
+            $(this).attr('maxlength', '3')
+            if (this.value.length >= 3) {
+                $(this).next().focus()
+                this.max
+            }
+            break                
+        }
+        case 'C_WorkPhone3':
+        case 'C_HomePhone3':
+            $(this).attr('maxlength', '4')
+            if (this.value.length >= 4) {
+                $(this).next().focus()
+                this.max
+            }
+            break                
+        }
+    }
+
+            
+
+    //fills out forms based on cookies
+    $.each($.cookie(), function (index, item) {
+         $('input').each(function() {
+            if ($(this).attr('name') == index)
+               // $('input').value = item
+                $('input[name=' + index + ']').val(item)
+        });
+    });
+    $("input").keyup(function() {
+        $.cookie($(this).attr('name'), this.value, { path: '/' }))
+        //console.log($.cookie())      
+
+
+    // any fields you wish to ignore, just stick a 'class=ignore' in the input type HTML
+    $.validator.setDefaults({ignore: ".ignore"});
+    $('#submit').click(function () {
+        $("#LeadForm").validate(opt)
+    });
+    $('#next').click(function () {
+        $("#LeadForm").validate(opt);  //validate whole form
+        var fsID = $(this).closest('fieldset').attr('id');        
+        $('#' + fsID + ' :input').each(function (index, element) {
+            //iterate through elments in fieldset
+            var eleid = $(element).attr('id');
+            if (eleid !== 'next' && eleid !== 'submit') {
+                var valid = $("#LeadForm").validate().element( "#"+eleid );
+            }
+        });
+        return false;
+    });
+});
